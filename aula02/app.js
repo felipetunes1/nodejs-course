2878655ea5f540432762bba59ada08bb0e80e3bb
felipetunes1 <45576231+felipetunes1@users.noreply.github.com>
2018-12-05 07:32:56 -0200

    Delete app.js

--- aula02/app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.set('view engine', 'ejs');
-
-require("./rotas/produtos")(app);
-require("./rotas/home")(app);
-
-module.exports = app;
